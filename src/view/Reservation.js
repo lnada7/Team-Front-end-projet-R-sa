8f1f2dcd06fabd67f6a1ac7316b93fd0ef6d8954
Poups <62133231+TomPoupi@users.noreply.github.com>
2021-04-26 22:55:16 +0200

    Add files via upload
    j'ai modif la maniere de mettre les descriptif d'un élement avec moin de css plus simple niveau code mais pas finit

--- src/view/Reservation.js
+++ src/view/Reservation.js
@@ -13,11 +13,22 @@ import { Link as RouterLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
 
+
+const nom = 'Crêpière';
+const description = "Crêpière classique pour faire réchauffer des crêpes. Dimension 40 x 12 cm. Attention le câcle d'alimentation se décroche un peu";
+const type = 'Electromenager';
+const assos = 'Carnaval Humanitaire';
+const statut = true;
+const caution = 50;
+
+
 const useStyles = makeStyles((theme) =>
 	({
+		
+
 		CadreSousTitre:{
 			position:'relative',
-			top:'50px',
+			marginTop:theme.spacing(3),
 			backgroundColor:'#4888EA',
 			width: '75%',
 			height: '100px',
@@ -26,7 +37,7 @@ const useStyles = makeStyles((theme) =>
 
     	SousTitre:{
     		position:'relative',
-			top:'25px',
+			top:'25%',
 	    	fontSize:'28px',
 	    	fontWeight:'bold',
 	    	color: 'white',
@@ -34,18 +45,17 @@ const useStyles = makeStyles((theme) =>
 	    	lineHeight: '1.5',
     	},
 
-		CadreExterieurDescription:{
+		CadreExterieur:{
 			position:'relative',
-			top:'100px',
+			marginTop:theme.spacing(3),
 			backgroundColor:'#D3E6F0',
 			width: '100%',
-			height: '800px',
 			margin: 'auto',
     	},
 
 		CadreNomElement:{
 			position:'relative',
-			top:'5%',
+			top:theme.spacing(2),
 			right:'35%',
 	    	backgroundColor:'#FFFFFF',
 	    	width:'20%',
@@ -64,154 +74,84 @@ const useStyles = makeStyles((theme) =>
 
     	},
 
-		CadreInterieurDescription:{
+		CadreInterieur:{
 			position:'relative',
-			top:'10%',
-			backgroundColor:'#FFFFFF',
-			width: '85%',
-			height: '65%',
-			margin: 'auto',
+			top:theme.spacing(2),
+			width:'95%',
+			margin:'auto',
+			
     	},
 
-    	CadreTexteDescription:{
+    	CadreBouton:{
 			position:'relative',
-			top:'5%',
-			//right:'10%',
-	    	backgroundColor:'#AAFFA3',
-	    	width:'90%',
-	    	height:'20%',
-			margin: 'auto',
+			paddingTop:theme.spacing(2),
+			//paddingLeft:theme.spacing(20),
+			width:'95%',
+			//margin:'auto',
+			
     	},
 
-    	TexteDescription:{
-    		position:'relative',
-			top:'10%',
-			left:'1.5%',
-			textAlign:'left',
-	    	fontSize:'15px',
-	    	//fontWeight:'bold',
-	    	color: '#000000',
+		CadreElement:{
+			width:'100%',
+			margin:'auto',
+			backgroundColor:'white',
     	},
 
-    	CadreTexteType:{
-			position:'relative',
-			top:'10%',
-			//right:'10%',
-	    	backgroundColor:'#D3E6F0',
-	    	width:'90%',
-	    	height:'10%',
-			margin: 'auto',
-    	},
 
-    	TexteType:{
-    		position:'relative',
-			top:'10%',
-			left:'1.5%',
-			textAlign:'left',
-	    	fontSize:'15px',
-	    	//fontWeight:'bold',
-	    	color: '#000000',
-    	},
-
-    	CadreTexteAssos:{
-			position:'relative',
-			top:'15%',
-			//right:'10%',
-	    	backgroundColor:'#ffa2a2',
-	    	width:'90%',
-	    	height:'10%',
-			margin: 'auto',
-    	},
-
-    	TexteAssos:{
-    		position:'relative',
-			top:'10%',
-			left:'1.5%',
-			textAlign:'left',
-	    	fontSize:'15px',
-	    	//fontWeight:'bold',
-	    	color: '#000000',
-    	},
-
-    	CadreDisponible:{
-			position:'relative',
-			top:'20%',
-			right:'35%',
-	    	backgroundColor:'#D1D1D1',
-	    	width:'20%',
-	    	height:'15%',
-			margin: 'auto',
-    	},
-
-    	Disponible:{
-    		position:'relative',
-			top:'15%',
-			left:'1.5%',
-			textAlign:'left',
-	    	fontSize:'15px',
-	    	//fontWeight:'bold',
-	    	color: '#000000',
-    	},
-
-    	CadreTexteCaution:{
-			position:'relative',
-			top:'5%',
-			left:'35%',
-	    	backgroundColor:'#D1D1D1',
-	    	width:'20%',
-	    	height:'10%',
-			margin: 'auto',
-    	},
-
-    	TexteCaution:{
-    		position:'relative',
-			top:'15%',
-			left:'1.5%',
-			textAlign:'left',
-	    	fontSize:'15px',
-	    	//fontWeight:'bold',
-	    	color: '#000000',
-    	},
-
-    	CadreQuantite:{
-			position:'relative',
-			top:'15%',
-			left:'35%',
-	    	backgroundColor:'#D1D1D1',
-	    	width:'20%',
-	    	height:'10%',
-			margin: 'auto',
-    	},
-
-    	Quantite:{
-    		position:'relative',
-			top:'15%',
-			left:'1.5%',
-			textAlign:'left',
-	    	fontSize:'15px',
-	    	//fontWeight:'bold',
-	    	color: '#000000',
-    	},
 
     	BoutonRetour:{
-
+    		//marginTop:'0%',
+    		//marginLeft: '50%',
     	},
 
       	BoutonReserver:{
+      		//marginLeft: '50%',
 
     	},
 
-    	Vert:{
-    		color:'green',
-    	},
 
-    	Red:{
-    		color:'red',
-    	},
 
 
 	})
 );
+
+function disponible(statut){
+  return (statut === true ? "Disponible" : "Non-Disponible");
+}
+const Dispo = disponible(statut)
+
+const OBJET_RESERVER_LINK = [
+    {
+        name: "Description",
+        detail: description,
+        
+    },
+
+    {
+        name: "Type",
+        detail: type,
+        
+    },
+
+    {
+        name: "Assos",
+        detail: assos,
+        
+    },
+
+    {
+        name: "statut",
+        detail: Dispo,
+       
+    },
+
+    {
+        name: "Caution",
+        detail: caution + "$",
+        
+    },
+
+];
 function Disponible(statut){
 	const classes = useStyles();
 	return (statut === true ? <div className={classes.Vert}>Disponible</div> : <div className={classes.Red}>Non-Disponible</div>);
@@ -219,91 +159,62 @@ function Disponible(statut){
 
 
 function Reservation(){
-	
 	const classes = useStyles();
-	const nom = 'Crêpière';
-	const description = "Crêpière classique pour faire réchauffer des crêpes. Dimension 40 x 12 cm. Attention le câcle d'alimentation se décroche un peu";
-	const type = 'Electromenager';
-	const assos = 'Carnaval Humanitaire';
-	const statut = true;
-	const caution = 50;
-
 	return(
-		<div>
-
-
+		<div className = {classes.root}>
 				<div className={classes.CadreSousTitre}>
 					<div className={classes.SousTitre}>
 						RESERVER UN ELEMENT
 					</div>
 				</div> 
 
-				<div className={classes.CadreExterieurDescription}>
+				<div className={classes.CadreExterieur}>
 					<div className={classes.CadreNomElement}>
 						<div className={classes.NomElement}>
 							{nom}
 						</div>
 					</div>
 
-					<div className={classes.CadreInterieurDescription}>
-
-						<div className={classes.CadreTexteDescription}>
-							<div className={classes.TexteDescription}>
-								<strong>Description</strong>: {description}
-							</div>
-						</div>
-
-						<div className={classes.CadreTexteType}>
-							<div className={classes.TexteType}>
-								<strong>Type</strong>: {type}
-							</div>
-						</div>
-
-						<div className={classes.CadreTexteAssos}>
-							<div className={classes.TexteAssos}>
-								<strong>Assos</strong>: {assos}
-							</div>
-						</div>
-
-						<div className={classes.CadreDisponible}>
-							<div className={classes.Disponible}>
-								<strong>Statut</strong>: {Disponible(statut)}
-							</div>
-						</div>
-
-						<div className={classes.CadreTexteCaution}>
-							<div className={classes.TexteCaution}>
-								<strong>Caution</strong>: {caution} €/pcs
-							</div>
-						</div>
-
-						<div className={classes.CadreQuantite}>
-							<div className={classes.Quantite}>
-								<strong>Quantité</strong>:
-							</div>
-						</div>						
-
+					<div className = {classes.CadreInterieur}>
+				
+						<List>
+						  {OBJET_RESERVER_LINK.map((element) => {
+						      return (
+						          <ListItem key={"list_" + element.name}  >
+						            <div className = {classes.CadreElement}>
+						              <ListItemText
+						                  primary={element.name}
+						                  secondary={element.detail}
+						              />
+						            </div>
+						          </ListItem>               
+						      );
+						  })}
+						</List>
 					</div>
+					<div className = {classes.CadreBouton}>
+						<Button
+						component={RouterLink}
+						to="/resa"
+						variant="contained"
+						color="secondary"
+						className={classes.BoutonRetour}
+						>
+						  <strong>RETOUR</strong> 
+						</Button> 
 
-				   	<Button
-				   	component={RouterLink}
-				   	to="/resa"
-				   	variant="contained"
-			    	color="secondary"
-			    	className={classes.button}
-			    	>
-				   		<strong>RETOUR</strong> 
-				    </Button>	
+						<Button
+						component={RouterLink}
+						variant="contained"
+						color="primary"
+						className={classes.BoutonReserver}
+						>
+						  <strong>RESERVER</strong> 
+						</Button>
+					</div>
+		           </div> 				
 
-				   	<Button
-				   	component={RouterLink}
-				   	variant="contained"
-			    	color="primary"
-			    	className={classes.button}
-			    	>
-				   		<strong>RESERVER</strong> 
-				    </Button>					    				
-				</div>
+
 
 		</div>
 
